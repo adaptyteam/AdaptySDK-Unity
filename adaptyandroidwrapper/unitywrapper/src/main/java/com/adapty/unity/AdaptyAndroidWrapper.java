aa4797f432217d7c61b2c254f306b2c41e2f33b5
vlad <vlad@vg.codes>
2025-04-22 20:50:50 +0400

    upgrade with android plugin version 3.4.0

--- adaptyandroidwrapper/unitywrapper/src/main/java/com/adapty/unity/AdaptyAndroidWrapper.java
+++ adaptyandroidwrapper/unitywrapper/src/main/java/com/adapty/unity/AdaptyAndroidWrapper.java
@@ -9,26 +9,38 @@ import com.unity3d.player.UnityPlayer;
 
 public class AdaptyAndroidWrapper {
 
+    private static CrossplatformHelper helper = null;
+
+    private static final Object lock = new Object();
+
     public static CrossplatformHelper getHelper() {
-        if (CrossplatformHelper.init(
-                UnityPlayer.currentActivity.getApplicationContext(),
-                (eventName, eventData) -> {
-                    runOnUnityThread(() -> {
-                        if (messageHandler != null) {
-                            messageHandler.onMessage(eventName, eventData);
-                        }
-                    });
-                },
-                value -> {
-                    int lastIndex = value.lastIndexOf("!/assets/");
-                    String result = (lastIndex != -1) ? value.substring(lastIndex + 9) : value;
-                    return FileLocation.fromAsset(result);
-                }
-            )
-        ) {
-            CrossplatformHelper.getShared().setActivity(() -> UnityPlayer.currentActivity);
+        if (helper != null)
+            return helper;
+
+        synchronized (lock) {
+            if (helper != null)
+                return helper;
+            if (CrossplatformHelper.init(
+                    UnityPlayer.currentActivity.getApplicationContext(),
+                    (eventName, eventData) -> {
+                        runOnUnityThread(() -> {
+                            if (messageHandler != null) {
+                                messageHandler.onMessage(eventName, eventData);
+                            }
+                        });
+                    },
+                    value -> {
+                        int lastIndex = value.lastIndexOf("!/assets/");
+                        String result = (lastIndex != -1) ? value.substring(lastIndex + 9) : value;
+                        return FileLocation.fromAsset(result);
+                    }
+                )
+            ) {
+                CrossplatformHelper.getShared().setActivity(() -> UnityPlayer.currentActivity);
+            }
+            helper = CrossplatformHelper.getShared();
+            return helper;
         }
-        return CrossplatformHelper.getShared();
     }
     private static Handler unityMainThreadHandler;
     private static AdaptyAndroidMessageHandler messageHandler;
